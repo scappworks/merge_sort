fcab11050e545d88163cf685cb082dfed27e37e6
scott <scappworks@gmail.com>
2020-10-14 04:04:08 -0400

    'merge_sort.rb'. 'merge_sort' function working properly. need to finish 'merge' function for split arrays.

--- merge_sort.rb
+++ merge_sort.rb
@@ -15,11 +15,30 @@ def merge_sort(arr)
     else
         new_array.push(merge_sort(arr.slice(0, arr.length / 2)))
         new_array.push(merge_sort(arr.slice(arr.length / 2, arr.length / 2)))
-        new_array.flatten!
+        #merge(arr.slice(0, arr.length / 2), arr.slice(arr.length / 2, arr.length / 2))
+        #new_array.flatten!
     end
 
-    
     return new_array
+end
+
+def merge(l,r)
+    compare_l = l[0]
+    compare_r = r[0]
+    new_array = []
+
+    if compare_l < compare_r
+        new_array.push(compare_l)
+        l.shift
+    elsif compare_r < compare_l
+        new_array.push(compare_r)
+        r.shift
+    else
+        new_array.push(compare_l)
+        l.shift
+    end
+
+    print "merge " + new_array.to_s + "\n"
 end
 
 print merge_sort([5,2,9,1,0,10,20,15]).to_s + "\n"
